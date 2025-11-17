2dfda89c431e0c2a6a318a2df086c2846afa91d2
ashnkay <xkayladleex@gmail.com>
2025-11-17 13:01:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+// Tab Switching
+const tabs = document.querySelectorAll(".tabs button");
+const sections = document.querySelectorAll(".tab");
 
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    const target = tab.dataset.tab;
+
+    sections.forEach(sec => sec.classList.remove("active"));
+    document.getElementById(target).classList.add("active");
+  });
+});
